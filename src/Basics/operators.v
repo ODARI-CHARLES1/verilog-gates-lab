7e37589fee7de6a6668dfb0be388d4f1b1c049cf
ODARI-CHARLES1 <daymondodari68@gmail.com>
2025-11-27 15:51:07 +0300

    inital commit operators in verilog

---
+++ src/Basics/operators.v
@@ -0,0 +1,9 @@
+/*Operators in verilog include 
+
+ 1. Logical Operators
+ 2. Relational Operators
+ 3. Arithmetic Operators
+ 4. Shift Operators
+ 5. Other Operators -Concatenate Operators {}
+                    -Replicate N times {N{}}
+*/
